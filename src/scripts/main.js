6c3d653ee0adc9645245e3b28afddb0157966ed3
maxmodrr <maxmodrr@gmail.com>
2024-10-21 17:59:29 +0300

    results

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
+document.addEventListener('click', (e) => {
+  const elem = e.target;
+  const lastChild = elem.lastElementChild;
+
+  if (lastChild.matches('ul')) {
+    lastChild.style.display =
+      getComputedStyle(lastChild).display === 'none' ? 'block' : 'none';
+  }
+});
 // write code here
