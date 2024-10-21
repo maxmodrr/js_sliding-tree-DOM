90f4c92d803e796b5c415dd4cb97508d2122ca46
maxmodrr <maxmodrr@gmail.com>
2024-10-21 17:59:53 +0300

    make build

---
+++ index.ac7d3324.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(e){var t=e.target.lastElementChild;t.matches("ul")&&(t.style.display="none"===getComputedStyle(t).display?"block":"none")});
+//# sourceMappingURL=index.ac7d3324.js.map
